d1bc5782fbf81bd54c57c7c2f5773bccbc88b250
jonielmendes <jonielmendes237@gmail.com>
2025-02-15 03:00:58 -0300

    Salvar progresso atual do projeto

---
+++ src/lib/interrupt.c
@@ -0,0 +1,30 @@
+#include "button.h"
+#include "interrupt.h"
+#include "matrix.h"
+#include "pico/bootrom.h"
+#include "pico/stdlib.h"
+#include <stdio.h>
+
+// Função que lida com eventos de interrupção
+void gpio_irq_handler(uint gpio, uint32_t events) {
+    // eventos de acionamento de botões
+    if(gpio == BUTTON_A || gpio == BUTTON_B || gpio == JOYSTICK_BUTTON) {
+
+        // tratamento de debouncing dos botões
+        if(!debouncing(300))
+            return;
+        // Habilita a entrada do modo BOOTSELL ao pressionar JOYSTICK_BUTTON
+        if(gpio == JOYSTICK_BUTTON)
+            reset_usb_boot(0,0);
+        // avança para o próximo número (máximo 9)
+        if(gpio == BUTTON_A && actual_number < 9)
+            actual_number++;
+        // recua para o número anterior (mínimo 0)
+        if(gpio == BUTTON_B && actual_number)
+            actual_number--;
+        // atualiza a matriz de leds
+        set_one_led(numbers[actual_number], led_r, led_g, led_b);
+        // log de depuração
+        printf("Contador: %d\n", actual_number);
+    }
+}
---
+++ src/main.c
@@ -0,0 +1,48 @@
+#include <stdio.h>
+
+#include "pico/stdlib.h"
+#include "hardware/pwm.h"
+#include "hardware/pio.h"
+
+#include "ws2812.pio.h"
+
+// biblioteca para manipulação de leds
+#include "lib/led.h"
+// biblioteca para manipulação de botões
+#include "lib/button.h"
+// biblioteca para manipulação de interrupções
+#include "lib/interrupt.h"
+// biblioteca para manipulação da matriz de leds
+#include "lib/matrix.h"
+
+// controla a intensidade da cor dos leds
+uint8_t led_r = 0;
+uint8_t led_g = 0;
+uint8_t led_b = 100;
+
+int main() {
+    stdio_init_all();
+    // Inicializa o led Azul
+    init_led(LED_BLUE);
+    
+    // Inicialização do botões e configuração de eventos de interrupção
+    init_button_with_interrupt(BUTTON_A, GPIO_IRQ_EDGE_FALL, true);
+    init_button_with_interrupt(BUTTON_B, GPIO_IRQ_EDGE_FALL, true);
+    init_button_with_interrupt(JOYSTICK_BUTTON, GPIO_IRQ_EDGE_FALL, true);
+  
+    PIO pio = pio0;
+    int sm = 0;
+    uint offset = pio_add_program(pio, &ws2812_program);
+
+    ws2812_program_init(pio, sm, offset, WS2812_PIN, 800000, IS_RGBW);
+    
+    // Inicialmente exibe o numero zero
+    set_one_led(numbers[actual_number], led_r, led_g, led_b);
+    while(true) {
+        // 5 ciclos de 200ms totalizando 1 segundo
+            // 100ms ligado
+            // 100ms desligado
+        blink_led(LED_BLUE, 100, 5);
+    }
+    return 0;
+}
